a6a20c90513401458d458fc334ce5f8d8bca4bcf
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-03-14 19:04:16 -0300

    Some fixes in songplay_table_insert and user_table_insert in sql_queries.py. Notebooks rerunned.

--- sql_queries.py
+++ sql_queries.py
@@ -54,10 +54,7 @@ songplay_table_insert = ("""INSERT INTO songplays(start_time
 												,session_id
 												,location
 												,user_agent)
-							VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-							ON CONFLICT(songplay_id)
-							DO UPDATE
-							SET level = excluded.level;""")
+							VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""")
 
 user_table_insert = ("""INSERT INTO users(user_id,
                                           first_name,
@@ -65,7 +62,9 @@ user_table_insert = ("""INSERT INTO users(user_id,
                                           gender,
                                           level)
 							VALUES(%s, %s, %s, %s, %s)
-							ON CONFLICT (user_id) DO NOTHING;""")
+							ON CONFLICT (user_id)
+								DO UPDATE
+									SET level = excluded.level""")
 
 
 song_table_insert = ("""INSERT INTO songs(song_id
