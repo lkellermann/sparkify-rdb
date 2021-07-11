b00e61bfd0bd7e78f6aebc3a05dbc76495fbec30
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-07-10 21:51:21 -0300

    fix: changed create statement files to mnt/ and addapted to run them with bash (refac).

---
+++ mnt/create/users.sql
@@ -0,0 +1,21 @@
+/***************************************************************************
+ * Filename: /run/media/kellermann/files/git_repo/sparkify-rdb/src/create/users.sql
+ * Path: /run/media/kellermann/files/git_repo/sparkify-rdb/src/create
+ * Created Date: Saturday, July 10th 2021, 3:08:51 pm
+ * Author: lkellermann
+ * 
+ * Copyright (c) 2021 CompanyName
+ *
+ * Create statement for users dimension table.
+ ***************************************************************************/
+
+begin;
+create table if not exists 
+users(
+	user_id smallint primary key
+	,first_name varchar not null
+	,last_name varchar not null
+	,gender varchar
+	,level varchar
+);
+commit;
